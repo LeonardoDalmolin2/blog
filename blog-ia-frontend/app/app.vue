7121829b7d3bb75b6ad764825bad965537c1fa8e
Leo <leonardodalmolin.ld@gmail.com>
2025-07-22 02:03:18 -0300

    fix: configura import do Tailwind

--- blog-ia-frontend/app/app.vue
+++ blog-ia-frontend/app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-blue-500 text-white p-4">
+    Teste Tailwind CSS
   </div>
 </template>
--- blog-ia-frontend/nuxt.config.ts
+++ blog-ia-frontend/nuxt.config.ts
@@ -1,5 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  css: [
+    '../assets/css/tailwind.css'
+  ],
+  vite: {
+    plugins: [
+      viteTsconfigPaths()
+    ]
+  }
 })
